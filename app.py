3583c2bab76f75752f0cc50d8100e6883e62956f
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-05-22 00:46:48 -0400

    finished Create part of CRUD app

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://armanyadalmia:armanyadalmia@localhost:5433/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'List Item 1'
-    },{
-        'description': 'List Item 2'
-    },{
-        'description': 'List Item 3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
