5ced26612bbb2a302df83a438e0654230d2f880f
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-06-02 23:41:39 -0400

    Finished setting up migrations

--- app.py
+++ app.py
@@ -10,15 +10,18 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
-def __repr__(self):
-    return f'<Todo {self.id} {self.description}>'
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+# Before migrations we needed this
+# db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
